d7992af1dc284e1999d1b25612a49e2d9b53a047
Anand-Ochir <anadaochir@gmail.com>
2022-10-27 18:30:13 +0800

    feat: add post

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import PostList from "./pages/PostList";
 import PostItem from "./pages/PostItem";
+import { AddPost } from "./pages/AddPost";
 
 function App() {
   return (
@@ -11,11 +12,15 @@ function App() {
             <li>
               <Link to="/">Home</Link>
             </li>
+            <li>
+              <Link to="/add_post">Add Post</Link>
+            </li>
           </ul>
         </div>
         <Routes>
           <Route path="/" element={<PostList />} />
           <Route path="/posts/:postId" element={<PostItem />} />
+          <Route path="/add_post" element={<AddPost />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/AddPost.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { API } from "../utils/axios";
+
+
+export const AddPost = () => {
+  const [post, setPost] = useState({
+    title: '',
+    body: '',
+    coverImage: '',
+    userId: ''
+  });
+  const [res, setRes] = useState('')
+  
+  const add = async () => {
+    const { data } = await API.post('posts', {
+      ...post
+    })
+    if(data.error) {
+      setRes(data.error.message)
+    } else {
+      setRes('post added')
+    }
+  }
+
+  return (
+    <div>
+      <h1>Add Post</h1>
+      
+      <input placeholder="title" value={post.title} onChange={(e) => setPost({...post, title: e.target.value})} />
+      <input placeholder="body" value={post.body} onChange={(e) => setPost({ ...post, body: e.target.value })} />
+      <input placeholder="cover image link" value={post.coverImage} onChange={(e) => setPost({ ...post, coverImage: e.target.value })} />
+      <input placeholder="authorId" value={post.userId} onChange={(e) => setPost({ ...post, userId: e.target.value })} />
+
+      <button onClick={add} >Add</button>
+
+      <h3>{res}</h3>
+
+    </div>
+  )
+}
